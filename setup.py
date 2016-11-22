28a73c7534c29b2c24abaf97ae999effdcd2ccf1
tillahoffmann <t.hoffmann13@imperial.ac.uk>
2016-11-22 14:04:12 +0000

    Basic setup.py.

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name='python_skeleton',
     version='0.1',
-    url='https://github.com/tillahoffmann/python_skeleton',
     author='Till Hoffmann',
-    author_email='tillahoffmann@gmail.com',
     packages=find_packages(),
 )
---
+++ setup_skeleton.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from argparse import ArgumentParser
+
+
+def __main__():
+    ap = ArgumentParser('setup_skeleton')
+    group = ap.add_mutually_exclusive_group(required=True)
+    group.add_argument('name', help='name of the python package', nargs='?')
+    group.add_argument('--reset', '-r', action='store_true', help='reset the repository to HEAD')
+    ap.add_argument('--author', '-a', help='author of the package', default='Till Hoffmann')
+    ap.add_argument('--version', '-v', default='0.1')
+    args = ap.parse_args()
+
+    if args.reset:
+        raise NotImplementedError
+    else:
+        # Define the template for setup.py
+        template = """from setuptools import setup, find_packages
+
+
+setup(
+    name='{name}',
+    version='{version}',
+    author='{author}',
+    packages=find_packages(),
+)
+"""
+        setup_py = template.format(name=args.name, version=args.version, author=args.author)
+        with open('setup.py', 'w') as fp:
+            fp.write(setup_py)
+
+        print("Created setup.py.")
+
+
+
+if __name__ == '__main__':
+    __main__()
